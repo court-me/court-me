c76571457aeae3e30c1a0f25ba7de1e55c65dd0e
Rob Mayo <rbrtm984@gmail.com>
2023-09-23 12:27:42 -0400

    changed file structure and added containers/presentationals for favs/leastFavs

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import favoritesReducer from '../features/favorites/favoritesSlice'
+import favoritesReducer from '../features/favoritesContainer/favoritesSlice'
+import leastFavoritesReducer from '../features/leastFavoritesContainer/leastFavoritesSlice'
 
 export const store = configureStore({
   reducer: {
     favorites: favoritesReducer,
+    leastFavorites: leastFavoritesReducer
   },
 })
--- client/src/features/App.jsx
+++ client/src/features/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Favorites from './favorites/favorites.jsx';
+import Favorites from './favoritesContainer/favoritesContainer.jsx';
+import LeastFavorites from './leastFavoritesContainer/leastFavoritesContainer.jsx'
 
 const App = () => {
   return (
   <div id="mainContainer">
     <Favorites />
+    <LeastFavorites />
   </div>
 );
 };
