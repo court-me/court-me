c28b7a4ee008213c2ecec214be4aeecec0f961b7
Rob Mayo <rbrtm984@gmail.com>
2023-09-23 14:55:08 -0400

    moved map to component and managing map with redux store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import favoritesReducer from '../features/favoritesContainer/favoritesSlice'
 import leastFavoritesReducer from '../features/leastFavoritesContainer/leastFavoritesSlice'
+import mapReducer from '../features/map/mapSlice'
 
 export const store = configureStore({
   reducer: {
+    map: mapReducer,
     favorites: favoritesReducer,
     leastFavorites: leastFavoritesReducer
   },
