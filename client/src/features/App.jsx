a31264345c8657c9b1999c89ae5937257d6e6e45
Rob Mayo <rbrtm984@gmail.com>
2023-09-22 20:16:33 -0400

    added favorites component

--- client/src/features/App.jsx
+++ client/src/features/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Favorites from './favorites/favorites.jsx';
 
 const App = () => {
   return (
   <div id="mainContainer">
+    
+    <Favorites />
   </div>
 );
 };
---
+++ client/src/features/favorites/favorites.jsx
@@ -0,0 +1,13 @@
+// component to render favorite tennis courts'
+
+import React from 'react';
+
+const Favorites = () => {
+    return (
+        <div id="favoritesContainer">
+            <h1>Favorite Tennis Courts</h1>
+        </div>
+    );
+}
+
+export default Favorites;
