1d07a0f49c4fad8e2bd819ad11d58d1fa41d5d0e
Chris Wardrip <chriswardrip@Chriss-MacBook-Pro.local>
2023-09-25 11:58:48 -0400

    Updated fetch post request within form button

--- client/src/features/searchForm/searchForm.jsx
+++ client/src/features/searchForm/searchForm.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addToResults } from '../results/resultsSlice.js';
+// import { response } from '../../../../server/server.js';
 
-const searchForm= (props) => {
+const searchForm = (props) => {
     const dispatch = useDispatch();
 
     const dummyResult = {
@@ -13,20 +14,38 @@ const searchForm= (props) => {
         zip: 'dumb',
     }
 
-    let onHandleSearch = () => dispatch( 
-        addToResults({
-            name: dummyResult.name,
-            address: dummyResult.address,
-            city: dummyResult.city,
-            state: dummyResult.state,
-            zip: dummyResult.zip,
-        })
-    );
+    let onHandleSearch = () => {
+        const inputVal = document.getElementById("inputSearch").value;
 
+        fetch("api/find/", {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({
+                zip: 10001
+            })
+        })
+            .then(response => {
+                return response.json()
+                    .then(response => {
+                        console.log('response', response)
+                    }
+                    )
+            })
+
+        return dispatch(
+            addToResults({
+                name: dummyResult.name,
+                address: dummyResult.address,
+                city: dummyResult.city,
+                state: dummyResult.state,
+                zip: dummyResult.zip,
+            })
+        );
+    }
     return (
         <div id="searchForm">
-            <input></input>
-            <button onClick= {onHandleSearch}>Search</button>
+            <input id="inputSearch"></input>
+            <button onClick={onHandleSearch}>Search</button>
         </div>
     );
 }
