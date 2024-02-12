7a083450910160d05a1a13f479c25a154ebe7fca
Rob Mayo <rbrtm984@gmail.com>
2024-02-11 23:45:07 -0500

    the app builds and displays on expo go. the sign in for email/password does not redirect to the protected screen after login. the google auth does not function correctly - error regarding client_id, stopped before looking into further

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-
+import './firebaseConfig';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
